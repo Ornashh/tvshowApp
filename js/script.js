2f97db621975947c3b118110a2585a0186a92915
Ornash <44943070+Ornashh@users.noreply.github.com>
2021-03-09 14:49:20 +0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
-const popularShowUrl = "https://api.themoviedb.org/3/discover/tv?api_key=39008b197a5755859d6786a809d485be&language=en-US&sort_by=popularity.desc&page=1";
-const imgUrl = 'http://image.tmdb.org/t/p/w1280';
-const searchUrl = 'https://api.themoviedb.org/3/search/tv?api_key=39008b197a5755859d6786a809d485be&language=en-US&page=1&query=';
+const popularShowUrl =
+  "https://api.themoviedb.org/3/discover/tv?api_key=39008b197a5755859d6786a809d485be&language=en-US&sort_by=popularity.desc&page=1";
+const imgUrl = "http://image.tmdb.org/t/p/w1280";
+const searchUrl =
+  "https://api.themoviedb.org/3/search/tv?api_key=39008b197a5755859d6786a809d485be&language=en-US&page=1&query=";
 
-const card = document.querySelector('.card');
-const modalEl = document.querySelector('.modal');
-const formEl = document.querySelector('.form');
-const searchEl = document.querySelector('.search');
+const card = document.querySelector(".card");
+const modalEl = document.querySelector(".modal");
+const formEl = document.querySelector(".form");
+const searchEl = document.querySelector(".search");
 
 async function getTvShow(url) {
-    const response = await fetch(url);
-    const responseData = await response.json();
+  const response = await fetch(url);
+  const responseData = await response.json();
 
-    // console.log(responseData);
-    showTv(responseData.results);
+  showTv(responseData.results);
 }
 
 getTvShow(popularShowUrl);
 
 function showTv(query) {
-    card.innerHTML = '';
+  card.innerHTML = "";
 
-    query.forEach(tvShow => {
-        let cardInnerEl = document.createElement('div');
-        cardInnerEl.classList.add('card-inner');
-        
-        cardInnerEl.innerHTML = `
+  query.forEach((tvShow) => {
+    let cardInnerEl = document.createElement("div");
+    cardInnerEl.classList.add("card-inner");
+
+    cardInnerEl.innerHTML = `
             <img 
                 src="${imgUrl + tvShow.poster_path}"
                 alt="${tvShow.name}"
@@ -32,14 +33,14 @@ function showTv(query) {
             <div class="card-name">
                 ${tvShow.name}
             </div>
-        `
+        `;
 
-        card.appendChild(cardInnerEl);
-        
-        let modalInnerEl = document.createElement('div');
-        modalInnerEl.classList.add('modal-inner');
+    card.appendChild(cardInnerEl);
 
-        modalInnerEl.innerHTML = `
+    let modalInnerEl = document.createElement("div");
+    modalInnerEl.classList.add("modal-inner");
+
+    modalInnerEl.innerHTML = `
             <div class="modal-btn">
                 <button type="button">&times;</button>
             </div>
@@ -54,7 +55,10 @@ function showTv(query) {
                     <span>${tvShow.vote_average}</span>
                     <h2>
                         ${tvShow.name}
-                        <div class="year">(${tvShow.first_air_date.slice(0,4)})</div>
+                        <div class="year">(${tvShow.first_air_date.slice(
+                          0,
+                          4
+                        )})</div>
                     </h2>
                 </div>
                 <div class="overview">
@@ -64,30 +68,29 @@ function showTv(query) {
                     Country: ${tvShow.origin_country[0]}
                 </div>
             </div>
-        `
+        `;
 
-        cardInnerEl.addEventListener('click', () => {
-            modalEl.appendChild(modalInnerEl);
-            modalEl.style.transform = 'scale(1)';
-            document.body.style.overflow = 'hidden';
-           
-            document.querySelector('.modal-btn button').addEventListener('click', () => {
-                modalEl.innerHTML = '';
-                modalEl.style.transform = 'scale(0)';
-                document.body.style.overflow = 'auto';
-            });
-            
+    cardInnerEl.addEventListener("click", () => {
+      modalEl.appendChild(modalInnerEl);
+      modalEl.classList.add("show-modal");
+      document.body.style.overflow = "hidden";
+
+      document
+        .querySelector(".modal-btn button")
+        .addEventListener("click", () => {
+          modalEl.innerHTML = "";
+          modalEl.classList.remove("show-modal");
+          document.body.style.overflow = "auto";
         });
     });
-
+  });
 }
 
-formEl.addEventListener('submit', (e) => {
-    e.preventDefault();
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if(searchEl.value) {
-        getTvShow(searchUrl + searchEl.value);
-        searchEl.value = '';
-    }
-    
-})
+  if (searchEl.value) {
+    getTvShow(searchUrl + searchEl.value);
+    searchEl.value = "";
+  }
+});
